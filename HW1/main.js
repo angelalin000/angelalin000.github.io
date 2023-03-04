331330dd8590851bc1e4c786c07ac50d62b84cba
Angela <angela911206@gmail.com>
2023-03-04 19:35:47 +0800

    'HW1'

---
+++ HW1/main.js
@@ -0,0 +1,17 @@
+
+var msg = "ただ今より、何かトラブルが起こっているようです。少々お待ちください。" +
+    "Sorry, it have some problem. Please wait a second."
+var interval = 170;
+var index = 0;
+var n = 1;
+
+function marquee() {
+    document.myForm.myText.value = msg.substring(index, msg.length) + msg.substring(0, msg.length);
+    index += n;
+    if (index > msg.length) {
+        index = 1;
+        n = 1;
+    }
+    if (index > 34) n = 2;
+    window.setTimeout("marquee();", interval);
+}
