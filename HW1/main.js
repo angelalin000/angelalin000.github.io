aab6f8b58bc792abf1792673fe092ea4025b25de
angelalin000 <91555222+angelalin000@users.noreply.github.com>
2023-03-04 22:16:15 +0800

    main.js

--- HW1/main.js
+++ HW1/main.js
@@ -1,4 +1,4 @@
-
+/*跑馬燈文字，因為公司是日本的，所以加了日文*/
 var msg = "ただ今より、何かトラブルが起こっているようです。少々お待ちください。" +
     "We are sorry,it seems something went wrong. Please wait a second."
 var interval = 170;
@@ -7,7 +7,9 @@ var n = 1;
 
 function marquee() {
     document.myForm.myText.value = msg.substring(index, msg.length) + msg.substring(0, msg.length);
+    /*因為日文一個字比英文寬，為了讓速度統一，英文的部分一次跑兩個字母，日文的部分一次跑一個字母，用 n 來分別*/
     index += n;
+    /*如果文字到底了就重新*/
     if (index > msg.length) {
         index = 1;
         n = 1;
