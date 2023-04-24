a130734dc90cbe4bf51f2e90fd2e0b38564c0402
angelalin000 <91555222+angelalin000@users.noreply.github.com>
2023-04-24 15:25:41 +0800

    HW3

---
+++ HW3/main.js
@@ -0,0 +1,16 @@
+
+function con_In(obj) {
+    obj.style.backgroundColor="#1b7f65";
+}
+
+function con_Out(obj) {
+    obj.style.backgroundColor="#26a182";
+}
+
+function cas_In(obj) {
+    obj.style.backgroundColor="#e5e5e5";
+}
+
+function cas_Out(obj) {
+    obj.style.backgroundColor="#ffffff";
+}
